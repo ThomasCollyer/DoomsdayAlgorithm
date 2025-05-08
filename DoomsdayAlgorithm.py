d6dbd64198026ec6ad4c97e840c9c4453fe15be6
ThomasCollyer <59969622+ThomasCollyer@users.noreply.github.com>
2025-05-08 19:16:21 +0100

    Doomsday Algorithm Version 0.6
    No meaningful changes in output, tidied some code and changed some logic.

--- DoomsdayAlgorithm.py
+++ DoomsdayAlgorithm.py
@@ -17,29 +17,79 @@ def this_doomsday(year):
     else:
         return '28/02/'+str(year)
 
+##def distance_from_doomday(day,month,year):
+##    days_to_doomsday = 0
+##
+##    for days in days_in_month:
+##
+##        if days == '01' and month=='01':
+##            days_to_doomsday += int(days_in_month['02']) + (int(days_in_month[month])-int(day))
+##            return days_to_doomsday
+##
+##        else:
+##            #We need to thin the dictionary down to only days we want
+##            new_days_in_month = {}
+##            for days in days_in_month:
+##                if days != '01' and int(days) <= int(month):
+##                    new_days_in_month.update({days:days_in_month[days]})
+##            #We want to get the number of days between the given date and doomsday
+##
+##            #Case for if the given date is in February
+##            if month == "02":
+##                days_to_doomsday = int(this_doomsday(year)[:2]) - int(day)
+##                return days_to_doomsday
+##            new_days_in_month.pop(month)
+##            new_days_in_month.pop("02")
+##
+##            for days_ in new_days_in_month:
+##                days_to_doomsday+= new_days_in_month[days_]
+##            days_to_doomsday += int(day)+1
+##            
+##            return days_to_doomsday
+
 def distance_from_doomday(day,month,year):
     days_to_doomsday = 0
 
-    for days in days_in_month:
+    if month=='01':
+        days_to_end_jan = 31-int(day)
+        days_to_doomsday = days_to_end_jan + int(days_in_month['02'])+1
+        return days_to_doomsday
+    elif month=='02':
+        days_to_doomsday = int(days_in_month['02']) - int(day)
+        return days_to_doomsday
+    else:
+        #Thin down dictionary to only relevant months
+        days_in_month.pop('01')
+        days_in_month.pop('02')
 
-        if days == '01' and month=='01':
-            days_to_doomsday += int(days_in_month['02']) + (int(days_in_month[month])-int(day))
-            return days_to_doomsday
-
-        else:
-            #We need to thin the dictionary down to only days we want
-            new_days_in_month = {}
-            for days in days_in_month:
-                if days != '01' and int(days) <= int(month):
-                    new_days_in_month.update({days:days_in_month[days]})
-            return new_days_in_month
-            #Pick up here
+        new_days_in_month = {}
+        
+        for months in days_in_month:
+            if int(months) == int(month):
+                break
+            else:
+                new_days_in_month.update({months:days_in_month[months]})
+        days_to_doomsday += int(day) + sum([new_days_in_month[month] for month in new_days_in_month])+3
+        
+        print(days_to_doomsday)
+        return days_to_doomsday
                 
     
+            
+def day_from_days():
+    #I need to add logic to get the anchor day for the century
+    print(days_of_week[distance_from_doomday(day,month,year)%7])
+    #return days_of_week[distance_from_doomday(day,month,year)]%7
+                
+#Cases
+#Jan-Feb, non leap year. Passes
+#Jan-Feb, leap year
+#Mar-Dec, non leap year. Passes
+#Mar-Dec, leap year. Fails
         
         
-
-input_date = '01/01/2005'
+print(64%7)
+input_date = '01/04/2004'
 
 day = input_date[:2]
 month = input_date[3:5]
@@ -57,11 +107,17 @@ days_in_month = {"01":31,
                  "10":31,
                  "11":30,
                  "12":31}
-
+days_of_week = {0:'Friday',
+                1:'Saturday',
+                2:'Sunday',
+                3:'Monday',
+                4:'Tuesday',
+                5:'Wednesday',
+                6:'Thursday'}
 
     
-print(distance_from_doomday(day,month,year))
-
+#print(distance_from_doomday(day,month,year))
+print(day_from_days())
 
 ##for days in days_in_month:
 ##    print(days)
@@ -73,7 +129,7 @@ print(distance_from_doomday(day,month,year))
 ##        new_days.update({days:days_in_month[days]})
 ##print(new_days)
 
-
+#print([days_in_month[days] for days in days_in_month)
 
 
 
